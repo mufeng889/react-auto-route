732bb7c3eff198995ba4d7cfc2b32965d97ff216
wang <1509326266@qq.com>
2024-08-24 20:09:44 +0800

    optimize(projects): remove useless code

--- src/unplugin.ts
+++ src/unplugin.ts
@@ -1,9 +1,8 @@
-import path from 'node:path';
 import { createUnplugin } from 'unplugin';
 import ElegantReactRouter from './route-core';
 import type { ElegantConstRoute, ElegantReactRouterOption } from './types';
 
-export default createUnplugin<Partial<ElegantReactRouterOption> | undefined>((options, _meta) => {
+export default createUnplugin<Partial<ElegantReactRouterOption> | undefined>((options) => {
   const ctx = new ElegantReactRouter(options);
 
   return [
@@ -20,23 +19,6 @@ export default createUnplugin<Partial<ElegantReactRouterOption> | undefined>((op
         }
       }
     },
-    {
-      name: '@ohh-899/inject-name',
-      enforce: 'pre',
-      transformInclude(id) {
-        const { cwd, pageDir } = ctx.elegantRouter.options;
-
-        const isInPageDir = id.startsWith(path.posix.join(cwd, pageDir));
-
-        if (!isInPageDir) return null;
-
-        const filePath = path.posix.join(cwd, pageDir);
-
-        const glob = id.replace(`${filePath}/`, '');
-
-        return ctx.elegantRouter.isMatchPageGlob(glob);
-      }
-    }
   ];
 });
 
